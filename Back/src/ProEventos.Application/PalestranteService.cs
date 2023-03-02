e58150859f68092983b4664e207dc9a72b954543
Everton <prestador-espresso.labs5@cmcapital.com.br>
2023-03-02 17:22:46 -0300

    Finalizando service palestrante

--- Back/src/ProEventos.Application/PalestranteService.cs
+++ Back/src/ProEventos.Application/PalestranteService.cs
@@ -7,41 +7,111 @@ namespace ProEventos.Application
     public class PalestranteService : IPalestranteService
     {
         private readonly IGeralPersist persistence;
-       
-        public PalestranteService(IGeralPersist persistence)
+        private readonly IPalestrantePersist palestrantePersist;
+
+        public PalestranteService(IGeralPersist persistence, IPalestrantePersist palestrantePersist)
         {
             this.persistence = persistence;
-           
+            this.palestrantePersist = palestrantePersist;
         }
 
-        public Task<Palestrante> AddPalestrantes(Palestrante model)
+        public async Task<Palestrante> AddPalestrantes(Palestrante model)
         {
-            throw new NotImplementedException();
+              try
+            {
+                persistence.Add<Palestrante>(model);
+                
+                if (await persistence.SaveChangesAsync())
+                 return await palestrantePersist.GetPalestranteByIdAsync(model.Id, false);
+
+                return null;
+            }
+            catch (Exception ex)
+            {                
+                throw new Exception(ex.Message); 
+            }
         }
 
-        public Task<bool> DeletePalestrante(int palestranteId)
+        public async Task<Palestrante> UpdatePalestrante(int palestranteId, Palestrante model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var palestrante =  await palestrantePersist.GetPalestranteByIdAsync(palestranteId, false);
+                if (palestrante == null) return null;
+
+                model.Id = palestrante.Id;
+
+                persistence.Update(model);
+                if (await persistence.SaveChangesAsync())            
+                    return await palestrantePersist.GetPalestranteByIdAsync(model.Id, false);
+                
+                return null;
+            }
+            catch (Exception ex)
+            {            
+                throw new Exception(ex.Message);
+            }   
         }
 
-        public Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos = false)
+        public async Task<bool> DeletePalestrante(int palestranteId)
         {
-           throw new NotImplementedException();
+             try
+            {
+                var palestrante =  await palestrantePersist.GetPalestranteByIdAsync(palestranteId, false);
+                if (palestrante == null) throw new Exception("palestrante não encontrado");
+
+                persistence.Delete<Palestrante>(palestrante);
+                return (await persistence.SaveChangesAsync());                              
+            }
+            catch (Exception ex)
+            {            
+                throw new Exception(ex.Message);
+            }          
         }
 
-        public Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos)
-        {
-            throw new NotImplementedException();
+        public async Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos = false)
+        {           
+            try
+            {
+                var palestrantes = await palestrantePersist.GetAllPalestrantesAsync(includeEventos);
+                if (palestrantes == null) return null;
+                
+                return palestrantes;
+            }
+            catch (Exception ex)
+            {                
+                throw new Exception(ex.Message);
+            }
         }
 
-        public Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool includeEventos)
+        public async Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var palestrante = await palestrantePersist.GetAllPalestrantesByNomeAsync(nome, includeEventos);
+                if (palestrante == null) return null;
+                
+                return palestrante;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        public Task<Palestrante> UpdatePalestrante(int palestranteId, Palestrante model)
+        public async Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool includeEventos = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var palestrantes = await palestrantePersist.GetPalestranteByIdAsync(palestranteId, includeEventos);
+                if (palestrantes == null) return null;
+                
+                return palestrantes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
