4df1ceb8374f162f681aec1a9ed95a8ca154708a
Everton <prestador-espresso.labs5@cmcapital.com.br>
2023-03-09 23:38:29 -0300

    Ajustando layout do projeto

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,11 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
 
   @Input() titulo: string = '';
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subtitulo: string = 'Desde 2023';
+  @Input() botaoListar: boolean = false;
 
   constructor() { }
 
